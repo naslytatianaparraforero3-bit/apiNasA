43fad757c743307542c2c3f051ddcd7f1a890db7
Nasly <hercifil@gmail.com>
2026-08-05 08:46:50 -0500

    se crea estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [], /* jsjsjs*/
   bootstrap: [AppComponent]
 })
 export class AppModule { }
